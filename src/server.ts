d816c25d2d1849c1bb0314d45ee24c66d91c56d4
Julian Coleman <jcoleman.engineering@gmail.com>
2021-10-04 20:34:42 -0700

    feat: use correct path to public folder

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { errors } from 'celebrate';
 import cors from 'cors';
 import { config } from 'dotenv';
 import express from 'express';
+import * as path from 'path';
 
 /**
  * Load environment variables
@@ -37,7 +38,7 @@ app.use(express.json());
 // enable parsing of application/x-www-form-urlencoded Content-Type
 app.use(express.urlencoded({ extended: false }));
 // serve files out of the `public/` directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 // expose server API routes
 app.use('/', controllers);
 // catch-all Celebrate validation errors
